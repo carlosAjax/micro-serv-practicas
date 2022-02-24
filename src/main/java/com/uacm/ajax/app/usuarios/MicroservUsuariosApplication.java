808d6c4c44a108f8c8337b9c2c1d451b57eb601b
ajax <ajaxajaxcarlos@gmail.com>
2022-02-23 18:38:24 -0600

    Se agrega configuración de cliente eureka en properties

--- src/main/java/com/uacm/ajax/app/usuarios/MicroservUsuariosApplication.java
+++ src/main/java/com/uacm/ajax/app/usuarios/MicroservUsuariosApplication.java
@@ -2,7 +2,8 @@ package com.uacm.ajax.app.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@EnableEurekaClient
 @SpringBootApplication
 public class MicroservUsuariosApplication {
 
